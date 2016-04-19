33dfbe018c8df49eff1133714ee9e403a2a4da4e
Bikash Khanal <student.khanal@hotmail.com>
2016-04-20 00:28:58 +1000

    Completed implementing gmail login.

--- GoogleTest/src/test/java/com/bikash/google/GoogleHomePage.java
+++ GoogleTest/src/test/java/com/bikash/google/GoogleHomePage.java
@@ -5,6 +5,11 @@ import org.testng.annotations.Test;
 import org.testng.Assert;
 
 import org.testng.annotations.BeforeTest;
+
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
@@ -24,7 +29,28 @@ WebDriver driver;
 		
 		Assert.assertEquals(driver.getTitle(), "Google");
 		
+		System.out.println("I am here");
+		
+		//Getting parent handle
+		String parentWindowHandler = driver.getWindowHandle();
+		String subWindowHandler = null;
+		
+		Set<String> handles = driver.getWindowHandles();
+		Iterator<String> iterator = handles.iterator();
+		
+		while(iterator.hasNext()){
+			subWindowHandler = iterator.next();
+		}
+		driver.switchTo().window(subWindowHandler);
+		driver.switchTo().window(parentWindowHandler);
+		
+		driver.manage().timeouts().implicitlyWait(1000, TimeUnit.SECONDS);
 		driver.findElement(By.id("gb_70")).click();
+		System.out.println("I am here too");
+		
+		driver.findElement(By.id("Email")).sendKeys("student.khanal@gmail.com");
+		driver.findElement(By.id("next")).click();
+		
 	}
   @BeforeTest
   public void beforeTest() {
