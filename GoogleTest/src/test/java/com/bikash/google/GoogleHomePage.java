0edc34be460399a86e07f4ffa2eca8b200ff4484
Bikash Khanal <bikash.khanal@testpoint.com.au>
2016-04-19 13:45:16 +1000

    Added google search textbox and button search

--- GoogleTest/src/test/java/com/bikash/google/GoogleHomePage.java
+++ GoogleTest/src/test/java/com/bikash/google/GoogleHomePage.java
@@ -2,6 +2,7 @@ package com.bikash.google;
 
 import org.testng.annotations.Test;
 import org.testng.annotations.BeforeTest;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterTest;
@@ -15,6 +16,8 @@ WebDriver driver;
 	@Test
   public void googleHome() {
 		driver.get("http://www.google.com");
+		driver.findElement(By.id("lst-ib")).sendKeys("Testing");
+		driver.findElement(By.name("btnk")).click();
 	}
   @BeforeTest
   public void beforeTest() {
